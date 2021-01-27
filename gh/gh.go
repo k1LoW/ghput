5736b33583ff6dbae9caadc06785d6fab4410938
k1LoW <k1lowxb@gmail.com>
2021-01-27 14:16:04 +0900

    Add --close-issues-using-title-match to close current issues

--- gh/gh.go
+++ gh/gh.go
@@ -286,6 +286,35 @@ func (g *Gh) CreateGist(ctx context.Context, fname string, public bool, in io.Re
 	return nil
 }
 
+func (g *Gh) CloseIssuesUsingTitleMatch(ctx context.Context, closeTitle string, ignoreNumber int) error {
+	if closeTitle == "" {
+		return nil
+	}
+	opts := &github.SearchOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
+	}
+	r, _, err := g.client.Search.Issues(ctx, fmt.Sprintf("%s state:open type:issue in:title repo:%s/%s", closeTitle, g.owner, g.repo), opts)
+	if err != nil {
+		return err
+	}
+	for _, i := range r.Issues {
+		if *i.Number == ignoreNumber {
+			continue
+		}
+		g.PutIssueComment(ctx, *i.Number, fmt.Sprintf("Closed when ghput created #%d.", ignoreNumber))
+		closed := "closed"
+		_, _, err := g.client.Issues.Edit(ctx, g.owner, g.repo, *i.Number, &github.IssueRequest{
+			State: &closed,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 type roundTripper struct {
 	transport   *http.Transport
 	accessToken string
